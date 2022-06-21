9244af81ca9e9cf6e6658976fb30fbd2a852d889
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-06-21 18:46:47 +0100

    ''

---
+++ app.js
@@ -0,0 +1,18 @@
+function Name(params) {
+    var name = params
+    return name
+}
+
+function Heights(params) {
+    var heights = params
+    return heights
+}
+
+function Country(params) {
+    var country = params
+    return country
+}
+
+console.log('Name:', Name('Peter Samuel'));
+console.log('Heights:', Heights(4.45), 'ft');
+console.log('Country:', Country('Nigeria'));
---
+++ calculator.js
@@ -0,0 +1,43 @@
+// BASIC CALCULATOR USING FUNCTIONS
+function Operations(a, b) {
+
+    addition = () => {
+        var result = a + b
+        console.log(`addition : ${result}`);
+        subtraction()
+
+    }
+
+    subtraction = () => {
+        var result = a - b
+        console.log(`subtraction : ${result}`);
+        multiplication()
+
+    }
+
+    multiplication = () => {
+        var result = a * b
+        console.log(`multiplication : ${result}`);
+        division()
+
+    }
+
+    division = () => {
+        var result = a / b
+        console.log(`division : ${result}`);
+        mod()
+
+    }
+
+    mod = () => {
+        var result = a % b
+        console.log(`mod : ${result}`);
+
+    }
+    addition()
+
+}
+
+
+
+Operations(5, 5);
